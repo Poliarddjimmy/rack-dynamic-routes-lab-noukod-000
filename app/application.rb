b60b1e8258fdc8ad97f3a0e713442389259073a2
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-22 19:31:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
       req = Rack::Request.new(env)
   
       if req.path.match(/items/)
-        @@items.each do |item|
+        items.each do |item|
           resp.write "#{item}\n"
         end
       elsif req.path.match(/search/)
