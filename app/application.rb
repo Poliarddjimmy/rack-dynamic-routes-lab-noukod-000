62b599d701aff014870795859e5a7d55f64e5a8e
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-22 19:54:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,14 +6,13 @@ class Application
     req = Rack::Request.new(env)
  
     if req.path.match(/items/)
-        
-      
         @@items.each do |item|
           if item 
             resp.write "#{item.price}\n"
           else 
             resp.write "Item not found"
             resp.status = 400
+          end
         end
     elsif req.path.match(/cart/)
       if @@cart.empty?
