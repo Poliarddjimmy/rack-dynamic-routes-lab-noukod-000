70036545928c7a1fe28689a069c575c7b76f6569
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-22 20:02:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,34 +1,28 @@
+
 class Application
-  @@items = ["Apple","Carrots","Pears"]
-  @@cart = []
+
+  @@items = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
-    if req.path.match(/items/)
-      item_name = req.path.split(/items/).last
-      itemo = @@item.find{|i| i.name == item_name}
 
-        if itemo.nil?
-          resp.write "Item not found"
-          resp.status = 400
-        else
-          resp.write resp.price
-        end
-    elsif req.path.match(/cart/)
-      if @@cart.empty?
-        resp.write "Your cart is empty"
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+
+      item = @@items.find{|i| i.name == item_name}
+
+      if item.nil?
+        resp.write "Item not found"
+        resp.status = 400
       else
-        @@cart.each do |item|
-          resp.write "#{item}\n"
-        end
+        resp.write item.price
       end
-        
     else
       resp.write "Route not found"
       resp.status = 404
     end
- 
+
     resp.finish
   end
 end
